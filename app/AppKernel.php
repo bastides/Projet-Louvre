d26cdd70056bba1710a23c9c62593ea2f336bbad
bastides <bastide.sebastien@gmail.com>
2016-04-19 22:56:32 +0200

    Mise en place de la barre de reseaux sociaux via le bundle NomayaSocialButtonsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new LOUVRE\AppBundle\LOUVREAppBundle(),
             new Payum\Bundle\PayumBundle\PayumBundle(),
+            new Nomaya\SocialBundle\NomayaSocialBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
